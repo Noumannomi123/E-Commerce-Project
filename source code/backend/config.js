b5188512bdd2204c631f8a4f57524f8890f50970
Farhan Bukhari <fbukhari557@gmail.com>
2024-05-10 19:29:21 +0500

    package updates

--- source code/backend/config.js
+++ source code/backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 export const jwtSecret = "4564FAsfasJ4323"
-export const mongoDBURL = "mongodb://localhost:27017/E-Learning";
+export const mongoDBURL = "mongodb+srv://farhan:farhan1@cluster0.e66vzcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
